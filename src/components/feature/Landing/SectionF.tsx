73b312b048095c1d0b9d8c79f61cb9c448c6ce38
Adhil <adhil.mhdk28@gmail.com>
2024-10-27 17:24:02 +0530

    fixes

--- src/components/feature/Landing/SectionF.tsx
+++ src/components/feature/Landing/SectionF.tsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import Container from "@/components/common/Container";
 import Image from "next/image";
 import B1 from "./assets/B1";
@@ -5,48 +6,116 @@ import B2 from "./assets/B2";
 import B3 from "./assets/B3";
 import { LucideChevronLeft, LucideChevronRight } from "lucide-react";
 import Node from "@/assets/Node";
+import B from "./assets/B";
+import Circle from "./assets/Circle";
+
+const data = [
+    {
+        count: '01',
+        desc: 'Our curriculum offers a unique international experience that is tailored to the interests of our students.'
+    },
+    {
+        count: '02',
+        desc: 'International education experience allows students to grow as people and become more open-minded.'
+    },
+    {
+        count: '03',
+        desc: 'Our classes are taken by pilots, cabin crews, stewards, and other ground staff professionals.'
+    },
+    {
+        count: '04',
+        desc: 'We offer a wide range of courses that are designed to meet the needs of our students.'
+    },
+    {
+        count: '05',
+        desc: 'Our courses are designed to help students develop the skills they need to succeed in the aviation industry.'
+    },
+    {
+        count: '06',
+        desc: 'Our courses are designed to help students develop the skills they need to succeed in the aviation industry.'
+    },
+    {
+        count: '07',
+        desc: 'Our courses are designed to help students develop the skills they need to succeed in the aviation industry.'
+    }
+];
+
+function Box({ count, desc }: { count: string, desc: string }) {
+    return (
+        <div className="min-w-[327.5px] w-[327.5px] h-[177.5px] relative">
+            <B className="w-full h-full absolute" />
+            <div className="absolute w-[81.82px] h-[81.82px] mt-[15.02px] ml-[10.57px] flex justify-center items-center">
+                <Circle className="w-[81.82px] h-[81.82px] absolute" />
+                <span className="absolute z-10 text-[27.71px] leading-[44.35px] text-[#1B4A5E]">
+                    {count}
+                </span>
+            </div>
+            <div className="absolute right-0 w-[195px] h-full flex items-center justify-center pr-[11.5px] text-[#187593] font-wa font-[500] text-[16px] leading-[26px]">
+                {desc}
+            </div>
+        </div>
+    );
+}
 
 export default function SectionF() {
-    return <section className="w-full">
-        <Container className="items-center">
-            <span className="text-[#187593] text-[15.52px] lg:text-[22px] leading-[57px] font-[400]">Why Kairos Institute?</span>
-            <span className="text-[#187593] text-[29px] lg:text-[43px] leading-[57px] font-[400] text-center">Experience a New Era <br />of Learning</span>
-            <div className="flex flex-col lg:flex-row">
-                <div className="w-full md:w-[80%] md:h-[409px]">
-                    <Image
-                        src={'/images/landing/im_g.png'}
-                        alt="im_g"
-                        layout="fill"
-                        className="!static object-contain"
-                    />
-                </div>
-                <div className="flex-1">
-                    <p className="font-wa font-[500] text-[14px] leading-[26px] pt-5 text-[#5B5B5B]">
-                        Kairos Institute provides international exposure to students. Through international exposure, we ensure that our students elevate their credentials, promote professional and personal development, ensure confidence and independence, and have the opportunity to interact with global academia.
-                    </p>
-                </div>
-            </div>
-            <div className="flex w-full overflow-x-auto my-10 justify-center gap-x-10">
-                <B1 className="w-[327.5px] min-w-[327.5px] h-[177px]" />
-                <B2 className="w-[327.5px] min-w-[327.5px] h-[177px]" />
-                <B3 className="w-[327.5px] min-w-[327.5px] h-[177px]" />
-            </div>
+    const scrollRef = useRef<any>(null);
 
-            <div className="flex w-full">
-                <div className="flex-1 flex items-center">
-                    <Node />
-                    <div className="h-0.5 bg-[#2FC5E1] flex-1" />
+    const scrollLeft = () => {
+        if (scrollRef.current) {
+            scrollRef.current.scrollBy({ left: -300, behavior: "smooth" });
+        }
+    };
+
+    const scrollRight = () => {
+        if (scrollRef.current) {
+            scrollRef.current.scrollBy({ left: 300, behavior: "smooth" });
+        }
+    };
+
+    return (
+        <section className="w-full">
+            <Container className="items-center">
+                <span className="text-[#187593] text-[15.52px] lg:text-[22px] leading-[57px] font-[400]">Why Kairos Institute?</span>
+                <span className="text-[#187593] text-[29px] lg:text-[43px] leading-[57px] font-[400] text-center">Experience a New Era <br />of Learning</span>
+                <div className="flex flex-col lg:flex-row">
+                    <div className="w-full md:w-[80%] md:h-[409px] relative">
+                        <Image
+                            src={'/images/landing/im_g.png'}
+                            alt="im_g"
+                            layout="fill"
+                            className="!static object-contain"
+                        />
+                    </div>
+                    <div className="flex-1">
+                        <p className="font-wa font-[500] text-[14px] leading-[26px] pt-5 text-[#5B5B5B]">
+                            Kairos Institute provides international exposure to students. Through international exposure, we ensure that our students elevate their credentials, promote professional and personal development, ensure confidence and independence, and have the opportunity to interact with global academia.
+                        </p>
+                    </div>
                 </div>
-                <div className="w-5"></div>
-                <div className="flex gap-x-2">
-                    <button className="bg-[#2FC5E1] font-bold rounded-full w-[50px] h-[50px] flex justify-center items-center">
-                        <LucideChevronLeft size={25} color="#fff" />
-                    </button>
-                    <button className="bg-[#2FC5E1] font-bold rounded-full w-[50px] h-[50px] flex justify-center items-center">
-                        <LucideChevronRight size={25} color="#fff" />
-                    </button>
+                <div className="flex w-full overflow-x-auto my-10 gap-x-10" ref={scrollRef}>
+                    {
+                        data.map((d, i) => (
+                            <Box key={i} count={d.count} desc={d.desc} />
+                        ))
+                    }
                 </div>
-            </div>
-        </Container>
-    </section>
+
+                <div className="flex w-full">
+                    <div className="flex-1 flex items-center">
+                        <Node />
+                        <div className="h-0.5 bg-[#2FC5E1] flex-1" />
+                    </div>
+                    <div className="w-5"></div>
+                    <div className="flex gap-x-2">
+                        <button onClick={scrollLeft} className="bg-[#2FC5E1] font-bold rounded-full w-[50px] h-[50px] flex justify-center items-center">
+                            <LucideChevronLeft size={25} color="#fff" />
+                        </button>
+                        <button onClick={scrollRight} className="bg-[#2FC5E1] font-bold rounded-full w-[50px] h-[50px] flex justify-center items-center">
+                            <LucideChevronRight size={25} color="#fff" />
+                        </button>
+                    </div>
+                </div>
+            </Container>
+        </section>
+    );
 }
---
+++ src/components/feature/Landing/assets/B.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function B(props: React.SVGProps<SVGSVGElement>) {
+    return <svg {...props} width="329" height="179" viewBox="0 0 329 179" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M304.5 1H56.5C25.8482 1 1 25.8482 1 56.5V62.75C1 89.95 23.05 112 50.25 112H66.25C84.6135 112 99.5 126.887 99.5 145.25C99.5 163.613 114.387 178.5 132.75 178.5H304.5C317.755 178.5 328.5 167.755 328.5 154.5V25C328.5 11.7452 317.755 1 304.5 1Z" fill="#EDFDFE" stroke="#2FC5E1" />
+    </svg>
+
+
+}
